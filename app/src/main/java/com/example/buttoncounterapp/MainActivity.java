d8c74f804be87ca336db9ed06922927272b0d429
simonw101 <simonhanwilson@hotmail.co.uk>
2021-01-21 16:48:42 +0000

    lecture 37 java course

--- app/src/main/java/com/example/buttoncounterapp/MainActivity.java
+++ app/src/main/java/com/example/buttoncounterapp/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.buttoncounterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        textView.setMovementMethod(new ScrollingMovementMethod());
     }
 }
