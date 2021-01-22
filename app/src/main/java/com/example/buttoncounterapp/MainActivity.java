afce157cae9158f5c73abd1c4a9f8914f3fcc3f3
simonw101 <simonhanwilson@hotmail.co.uk>
2021-01-22 17:34:37 +0000

    lecture 42 java course

--- app/src/main/java/com/example/buttoncounterapp/MainActivity.java
+++ app/src/main/java/com/example/buttoncounterapp/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
+    private TextView textView;
+    private EditText userInput;
+    private Button button;
+    private int numTimesClicked = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
+        userInput = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+
+        userInput.setText("");
+
+        View.OnClickListener ourOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numTimesClicked += 1;
+                String result = "The Button got tapped" + numTimesClicked + " time";
+
+                if (numTimesClicked != 1) {
+
+                    result += "s";
+
+                }
+                result += "\n";
+                textView.append(result);
+            }
+        };
+
+        button.setOnClickListener(ourOnClickListener);
 
         textView.setMovementMethod(new ScrollingMovementMethod());
     }
